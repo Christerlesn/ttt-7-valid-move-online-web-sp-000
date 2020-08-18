a2958b3d48ff166e7b1b7654e0389972e697ef3d
Christerle Nounoute <christerlesn@gmail.com>
2020-08-18 16:19:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # check if the position is on the board
 #check if the position is not taken
 def valid_move?(board, index)
-if index.to_i.between?(0, 8)
+if index.to_i.between?(0, 8) == position_taken?(board, index)
   false
 end
 end
